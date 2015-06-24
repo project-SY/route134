ad35ebd95eb95e57a9b6852ac96b0fc0871fcc8b
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-06-24 10:35:35 +0900

    single.php

---
+++ single.php
@@ -0,0 +1,9 @@
+<?php include('header.php'); ?>
+	<div id="single" class="content-width">
+		<div class="contents-area clearfix">
+			<div id="pankuzu">
+			ぱんくずリストが入ります。
+			</div>
+		</div><!-- contents-area end -->
+	</div><!-- single end -->
+<?php include('footer.php'); ?>
