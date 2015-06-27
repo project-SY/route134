37df446381a00d311f96184c6b505eaab52ce8d8
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-06-27 09:49:07 +0900

    made single.php

--- single.php
+++ single.php
@@ -1,9 +1,78 @@
 <?php include('header.php'); ?>
-	<div id="single" class="content-width clearfix">
-		<div class="left-column">
-			<div id="pankuzu">
-			ぱんくずリストが入ります。
-			</div>
-		</div><!-- contents-area end -->
-	</div><!-- single end -->
+<div id="single" class="content-width clearfix">
+<div class="left-column">
+<div id="pankuzu" class="border-btm-gray">
+ぱんくずリストが入ります。
+</div>
+<div class="single__post-media clearfix border-btm-gray">
+<img class="post-img" src="./images/pic-headline-thumbnail-01.png">
+<div class="right">
+<h2>まるで海外のリゾート地♥ 湘南のオシャレなビーチハウスで週末を満喫しよう</h2>
+<p class="sub-title">『Pacific DRIVE-IN』は駐車場370台を備える七里ヶ浜海岸駐車場の中央に位置し、
+134号線沿いの海側に位置するドライブインカフェ。
+</p>
+</div>
+</div>
+<div class="sns-area border-btm-gray">
+<p>snsが入ります。</p>
+</div>
+<div class="post-main">
+<img src="./images/surfer.jpg">
+<h3>七里ヶ浜の海に最も近い「絶景ドライブインカフェ」オープン。</h3>
+<p>『Pacific DRIVE-IN』は駐車場370台を備える七里ヶ浜海岸駐車場の中央に位置し、134号線沿いの海側に
+位置するドライブインカフェ。ハワイのローカルが朝からドライブインで朝食を食べているリラックスした光景が
+この七里ガ浜の絶景と大きな駐車場、湘南のサーフカルチャーが根付くこの場所とリンクして、ドライブインに
+ぴったりだと思いプロジェクトがスタート。鎌倉・湘南・逗子のメンバーを中心にプロジェクトチームを編成しま
+した。
+</p>
+<h3>七里ヶ浜の海に最も近い「絶景ドライブインカフェ」オープン。</h3>
+<p>『Pacific DRIVE-IN』は駐車場370台を備える七里ヶ浜海岸駐車場の中央に位置し、134号線沿いの海側に
+位置するドライブインカフェ。ハワイのローカルが朝からドライブインで朝食を食べているリラックスした光景が
+この七里ガ浜の絶景と大きな駐車場、湘南のサーフカルチャーが根付くこの場所とリンクして、ドライブインに
+ぴったりだと思いプロジェクトがスタート。鎌倉・湘南・逗子のメンバーを中心にプロジェクトチームを編成しま
+した。
+</p>
+<h3>七里ヶ浜の海に最も近い「絶景ドライブインカフェ」オープン。</h3>
+<p>『Pacific DRIVE-IN』は駐車場370台を備える七里ヶ浜海岸駐車場の中央に位置し、134号線沿いの海側に
+位置するドライブインカフェ。ハワイのローカルが朝からドライブインで朝食を食べているリラックスした光景が
+この七里ガ浜の絶景と大きな駐車場、湘南のサーフカルチャーが根付くこの場所とリンクして、ドライブインに
+ぴったりだと思いプロジェクトがスタート。鎌倉・湘南・逗子のメンバーを中心にプロジェクトチームを編成しま
+した。
+</p>
+</div><!-- post-main end -->
+<div class="related-posts border-btm-gray">
+<h3>関連する記事</h3>
+<ul>
+<li class="media clearfix">
+<img src="">
+<div class="right">
+<h4>相模湾が一望できるシーサイドリゾートプール「大磯ロングビーチ...</h4>
+<p><span class="category">ライフスタイル</span>いろいろとアクティブなイベントが多い夏...</p>
+</div>
+</li>
+<li class="media clearfix">
+<img src="">
+<div class="right">
+<h4>相模湾が一望できるシーサイドリゾートプール「大磯ロングビーチ...</h4>
+<p><span class="category">ライフスタイル</span>いろいろとアクティブなイベントが多い夏...</p>
+</div>
+</li>
+<li class="media clearfix">
+<img src="">
+<div class="right">
+<h4>相模湾が一望できるシーサイドリゾートプール「大磯ロングビーチ...</h4>
+<p><span class="category">ライフスタイル</span>いろいろとアクティブなイベントが多い夏...</p>
+</div>
+</li>
+</ul>
+</div><!-- related-posts end -->
+<div class="sns-area border-btm-gray">
+<p>snsが入ります。</p>
+</div><!-- sns-area end -->
+<div class="auther-meta clearfix">
+<img src="">
+<p class="auther">Kazuma Ueda</p>
+</div><!-- auther-meta end -->
+</div><!-- l-column end -->
+</div><!-- single end -->
 <?php include('footer.php'); ?>
