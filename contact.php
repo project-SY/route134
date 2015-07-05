005d41abe007dceba9b1c97088835959043a6661
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-07-06 05:07:38 +0900

    modefied contact.php

--- contact.php
+++ contact.php
@@ -7,12 +7,19 @@
 				</div>
 				<h2>お問い合わせ</h2>
 				<p class="discription">Route134へのお問い合わせページです。</p>
-				<h3>Route134に関するお問い合わせ</h3>
+				<h3>Route134、広告掲載に関するお問い合わせ</h3>
+				<p class="notice">お問い合わせにつきましては、内容によっては対応できない場合がございます。恐れ入りますが、ご了承ください。</p>
 				<form class="contact-form">
-					<p><input type="text" name="name" maxlength="255" placeholder="name"></p>
-					<p><input type="text" name="email" maxlength="255" placeholder="email"></p>
-					<p><input type="text" name="subject" maxlength="255" placeholder="subject"></p>
 					<p>
+						<label>お名前<span class="required">必須</span></label>
+						<input type="text" name="name" maxlength="255" placeholder="name">
+					</p>
+					<p>
+						<label>メールアドレス<span class="required">必須</span></label>
+						<input type="text" name="name" maxlength="255" placeholder="name">
+					</p>
+					<p>
+						<label>内容<span class="required">必須</span></label>
 						<textarea name="message" rows="6" maxlength="255" placeholder="message"></textarea>
 					</p>
 					<p>
