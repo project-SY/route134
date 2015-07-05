c1308f27160749eae84ea70bca90f2966761fb35
Ken Sayama <sayamaken0402@gmail.com>
2015-07-05 11:35:34 +0900

    comp index.php

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 				いろいろとアクティブなイベントが多い夏。コーディネートの動きやす
 	さが重要となってくるけれど、やっ...
 			</p>
-			<div class="headline-info">
-				<div class="headline-category lifestyle">
+			<div class="category-info">
+				<div class="category-type lifestyle">
 					<h6>ライフスタイル</h6>
 				</div>
-				<div class="headline-author">
+				<div class="category-author">
 					<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
 					<h5>Ken Sayama</h5>
 				</div>
@@ -30,11 +30,11 @@
 				いろいろとアクティブなイベントが多い夏。コーディネートの動きやす
 	さが重要となってくるけれど、やっ...
 			</p>
-			<div class="headline-info">
-				<div class="headline-category lifestyle">
+			<div class="category-info clearfix">
+				<div class="category-type lifestyle">
 					<h6>ライフスタイル</h6>
 				</div>
-				<div class="headline-author">
+				<div class="category-author">
 					<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
 					<h5>Ken Sayama</h5>
 				</div>
@@ -66,7 +66,7 @@
 	<!--sidebar-colmun end-->
 </div>
 <div class="instragram-area">
-	<div class="instragram-area-inner content-width clearfix">
+	<div class="inner content-width clearfix">
 		<h4>湘南ライフをInstragramでシェア</h4>
 		<ul>
 			<li><a href="#"><img src="./images/pic-instragram-01.png" alt=""></a></li>
@@ -79,8 +79,151 @@
 			<li><a href="#"><img src="./images/pic-instragram-08.png" alt=""></a></li>
 		</ul>
 		<div class="instragram-area-info">
-			<i class="fa fa-camera"></i>
+			<a href="" style="margin-right: 20px;"><i class="fa fa-camera"></i>See&nbsp;More&nbsp;?</a>
+			<a href=""><i class="fa fa-heart"></i>あなたも写真を投稿しませんか？</a>
 		</div>
+	</div>
+</div>
+<div class="index-new-posts">
+	<div class="inner content-width clearfix">
+		<h4>最新の記事</h4>
+		<ul>
+			<li>
+				<a href="">
+					<img class="index-thumbnail" src="./images/pic-index-thumbnail-01.png" alt="">
+					<h5>動きやすさもかわいさもほしい！イベント別コーデ</h5>
+					<p>いろいろとアクティブなイベントが多い夏...</p>
+					<div class="category-info clearfix">
+						<div class="category-type lifestyle">
+							<h6>ライフスタイル</h6>
+						</div>
+						<div class="category-author">
+							<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
+							<h6>Ken Sayama</h6>
+						</div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="index-thumbnail" src="./images/pic-index-thumbnail-02.png" alt="">
+					<h5>大正時代に作られたという古民家
+をリノベーション。café recette</h5>
+					<p>いろいろとアクティブなイベントが多い夏...</p>
+					<div class="category-info clearfix">
+						<div class="category-type lifestyle">
+							<h6>ライフスタイル</h6>
+						</div>
+						<div class="category-author">
+							<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
+							<h6>Ken Sayama</h6>
+						</div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="index-thumbnail" src="./images/pic-index-thumbnail-03.png" alt="">
+					<h5>ラピュタの世界が鎌倉にあった！
+森に浮かぶ天空のカフェ「樹ガ...</h5>
+					<p>いろいろとアクティブなイベントが多い夏...</p>
+					<div class="category-info clearfix">
+						<div class="category-type lifestyle">
+							<h6>ライフスタイル</h6>
+						</div>
+						<div class="category-author">
+							<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
+							<h6>Ken Sayama</h6>
+						</div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="index-thumbnail" src="./images/pic-index-thumbnail-04.png" alt="">
+					<h5>藤沢駅南口で「はしご酒イベント」
+地元の飲食店に親しむきっかけに</h5>
+					<p>いろいろとアクティブなイベントが多い夏...</p>
+					<div class="category-info clearfix">
+						<div class="category-type lifestyle">
+							<h6>ライフスタイル</h6>
+						</div>
+						<div class="category-author">
+							<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
+							<h6>Ken Sayama</h6>
+						</div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="index-thumbnail" src="./images/pic-index-thumbnail-05.png" alt="">
+					<h5>荒れる湘南ビーチ「乱れる風紀」
+で“鎌倉市×海の家・財界”の攻防</h5>
+					<p>いろいろとアクティブなイベントが多い夏...</p>
+					<div class="category-info clearfix">
+						<div class="category-type lifestyle">
+							<h6>ライフスタイル</h6>
+						</div>
+						<div class="category-author">
+							<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
+							<h6>Ken Sayama</h6>
+						</div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="index-thumbnail" src="./images/pic-index-thumbnail-06.png" alt="">
+					<h5>相模湾が一望できるシーサイドリ
+ゾートプール「大磯ロングビーチ...</h5>
+					<p>いろいろとアクティブなイベントが多い夏...</p>
+					<div class="category-info clearfix">
+						<div class="category-type lifestyle">
+							<h6>ライフスタイル</h6>
+						</div>
+						<div class="category-author">
+							<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
+							<h6>Ken Sayama</h6>
+						</div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="index-thumbnail" src="./images/pic-index-thumbnail-07.png" alt="">
+					<h5>お手軽イタリアンの銘店「ラ・パッ
+トゥーラ」はいかがでしょう</h5>
+					<p>いろいろとアクティブなイベントが多い夏...</p>
+					<div class="category-info clearfix">
+						<div class="category-type lifestyle">
+							<h6>ライフスタイル</h6>
+						</div>
+						<div class="category-author">
+							<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
+							<h6>Ken Sayama</h6>
+						</div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img class="index-thumbnail" src="./images/pic-index-thumbnail-08.png" alt="">
+					<h5>湘南平塚フランス料理 マリール
+イーズ/Marie Louise にようこそ</h5>
+					<p>いろいろとアクティブなイベントが多い夏...</p>
+					<div class="category-info clearfix">
+						<div class="category-type lifestyle">
+							<h6>ライフスタイル</h6>
+						</div>
+						<div class="category-author">
+							<img src="./images/icon-index-author-thumbnail.png" height="15" width="15" alt="">
+							<h6>Ken Sayama</h6>
+						</div>
+					</div>
+				</a>
+			</li>
+		</ul>
 	</div>
 </div>
 
