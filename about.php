e25993111cbba7c20de8e6fad686961f981b22a4
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-07-06 06:12:13 +0900

    modefied about.php

--- about.php
+++ about.php
@@ -16,13 +16,35 @@
 						<img src="./images/pic-about-sunset.jpg" alt="">
 						<img src="./images/pic-about-sea.jpg" alt="">
 					</div>
-					<img class="about-kv" src="./images/pic-about-kv.jpg">
+					<img class="about-kv" src="./images/pic-about-kv.jpg" alt="">
 				</div>
 				<p class="discription">西は、大磯。東は逗子。人気スポット鎌倉や茅ヶ崎を囲んだ湘南エリアは、<br />
 					国道134号線を通じて自由に行き来することができます。<br />
 					Route134 は、国道134号線のように読者のみなさんと湘南エリアをつなぐ架け橋になりたいと思います。
 				</p>
-				<img class="message" src="./images/text-about-message.png">
+				<h1>編集部のご紹介</h1>
+				<p class="discription">西は、大磯。東は逗子。人気スポット鎌倉や茅ヶ崎を囲んだ湘南エリアは、
+					国道134号線を通じて自由に行き来することができます。
+					Route134 は、国道134号線のように読者のみなさんと湘南エリアをつなぐ架け橋になりたいと思います。
+				</p>
+				<div class="member-wrap clearfix">
+					<div class="member member_sayama">
+						<img src="./images/pic-about-member-thumbnail-01.png" alt="">
+						<h4>Ken Sayama</h4>
+						<p>神奈川県平塚市出身。Route134 チーフエンジニア。オールシーズンサーフィンして..</p>
+					</div>
+					<div class="member member_sayama">
+						<img src="./images/pic-about-member-thumbnail-02.png" alt="">
+						<h4>Ken Sayama</h4>
+						<p>神奈川県平塚市出身。Route134 チーフエンジニア。オールシーズンサーフィンして..</p>
+					</div>
+					<div class="member member_sayama">
+						<img src="./images/pic-about-member-thumbnail-03.png" alt="">
+						<h4>Ken Sayama</h4>
+						<p>神奈川県平塚市出身。Route134 チーフエンジニア。オールシーズンサーフィンして..</p>
+					</div>
+				</div>
+				<img class="message" src="./images/text-about-message.png" alt="">
 			</div>
 			<!-- about end -->
 		</div>
